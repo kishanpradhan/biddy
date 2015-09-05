639336c9e34f0aa6ad8f9c94d1494aed4ae8c44f
lokesh541 <lrbeeram@gmail.com>
2015-09-05 16:54:28 +0530

    fdvdgv

--- bids/PlaceBid.php
+++ bids/PlaceBid.php
@@ -5,3 +5,10 @@
  * Date: 5/9/15
  * Time: 2:03 PM
  */
+if(isset($_REQUEST['product_id']) && isset($_REQUEST['user_id']) && isset($_REQUEST['bid_price'])) {                    #submit change
+    $product_id = @$_REQUEST["product_id"];                  #name change
+    $user_id = @$_REQUEST["user_id"];
+    $bid_price = @$_REQUEST["bid_price"];                        #name change
+    $query = mysqli_query($db_conx, "INSERT INTO bids VALUES($id,$product_id,$user_id,$bid_price) ") or die(mysqli_error()); #table change
+
+}
