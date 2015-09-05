8144fa1241b8a3e05bcef313ef8aaaf389b466d9
lokesh541 <lrbeeram@gmail.com>
2015-09-05 16:22:20 +0530

    login commited

--- Authentication/login.php
+++ Authentication/login.php
@@ -6,14 +6,14 @@
  * Time: 1:53 PM
  */
 include_once('../connection.php');
-if(isset($_POST["submit"])){ #submit change
-    $email   = @$_POST["email"]; #name change
-    $passwd = @$_POST["pwd"];#name change
-    $sql = mysqli_query($db_conx,"SELECT * FROM users"); #table change
+if(isset($_REQUEST['email']) && isset($_REQUEST['pass'])){                    #submit change
+    $email   = @$_REQUEST["email"];                  #name change
+    $passwd = @$_REQUEST["pass"];                        #name change
+    $sql = mysqli_query($db_conx,"SELECT * FROM users where email = $email AND password = $passwd"); #table change
     while($rs = mysqli_fetch_array($sql)){
     if($rs["user"] == $email && $rs["passwd"] == $passwd ){  #name change
      $_SESSION["user"] = $email;
-     header('location:');   #inser redirect page
+     echo "success";
     }
     else
         echo "failure";
