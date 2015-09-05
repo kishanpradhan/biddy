55d2ead89898efef09b7e81f94dddfb984c56d3d
lokesh541 <lrbeeram@gmail.com>
2015-09-05 15:27:20 +0530

    login

--- .env.php
+++ .env.php
@@ -1,3 +1,3 @@
 <?php
 
-putenv("aa=123");
+putenv(' HOST =localhost')
--- Authentication/login.php
+++ Authentication/login.php
@@ -5,3 +5,17 @@
  * Date: 5/9/15
  * Time: 1:53 PM
  */
+include_once('../connection.php');
+$submit = $_POST["submit"]; #name to be renamed
+if($submit){
+    $email   = @$_POST["email"]; #name change
+    $passwd = @$_POST["pwd"];#name change
+    $sql = mysqli_query($db_conx,"SELECT * FROM users"); #table change
+}
+while($rs = mysqli_fetch_array($sql)){
+    if($email == $rs["user"] && $passd == $rs["passwd"]){  #name change
+        echo " success";
+    }
+    else
+        echo "failure";
+}
