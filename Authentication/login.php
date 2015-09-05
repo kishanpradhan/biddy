41dab0bdfb419bb275434413ade0bfaef015e3cd
lokesh541 <lrbeeram@gmail.com>
2015-09-05 16:34:56 +0530

    login commited

--- Authentication/login.php
+++ Authentication/login.php
@@ -7,15 +7,15 @@
  */
 include_once('../connection.php');
 if(isset($_REQUEST['email']) && isset($_REQUEST['pass'])){                    #submit change
-    $email   = @$_REQUEST["email"];                  #name change
+    $email  = @$_REQUEST["email"];                  #name change
     $passwd = @$_REQUEST["pass"];                        #name change
-    $sql = mysqli_query($db_conx,"SELECT * FROM users where email = $email AND password = $passwd"); #table change
-    while($rs = mysqli_fetch_array($sql)){
-    if($rs["user"] == $email && $rs["passwd"] == $passwd ){  #name change
+    $query = mysqli_query($db_conx,"SELECT * FROM users where email = $email AND password = $passwd") or die(mysqli_error()); #table change
+    $rs = mysqli_fetch_array($query) or die(mysqli_error());
+    if(!empty($rs["user"])  && !empty($rs["passwd"])){  #name change
      $_SESSION["user"] = $email;
      echo "success";
     }
     else
         echo "failure";
-}
+
 }
