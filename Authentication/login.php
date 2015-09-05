c28242b285801b7521bf97fe9951ad4db5bb618c
lokesh541 <lrbeeram@gmail.com>
2015-09-05 15:43:19 +0530

    jhkjh

--- Authentication/login.php
+++ Authentication/login.php
@@ -11,8 +11,7 @@ if($submit){
     $email   = @$_POST["email"]; #name change
     $passwd = @$_POST["pwd"];#name change
     $sql = mysqli_query($db_conx,"SELECT * FROM users"); #table change
-}
-while($rs = mysqli_fetch_array($sql)){
+    while($rs = mysqli_fetch_array($sql)){
     if($rs["user"] == $email && $rs["passwd"] == $passwd ){  #name change
      $_SESSION["user"] = $email;
      header('location:');   #inser redirect page
